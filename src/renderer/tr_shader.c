e93f5223feee184213e22c3fe3d175b050b38982
Kelly Anderson <kelly@silka.with-linux.com>
2011-04-07 16:50:41 -0600

    typecast to alphaGen_t, so Werror works, whitespace fixes

--- src/renderer/tr_shader.c
+++ src/renderer/tr_shader.c
@@ -2,9 +2,9 @@
 ===========================================================================
 
 Return to Castle Wolfenstein single player GPL Source Code
-Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company. 
+Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company.
 
-This file is part of the Return to Castle Wolfenstein single player GPL Source Code (RTCW SP Source Code).  
+This file is part of the Return to Castle Wolfenstein single player GPL Source Code (RTCW SP Source Code).
 
 RTCW SP Source Code is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -983,7 +983,7 @@ static qboolean ParseStage( shaderStage_t *stage, char **text ) {
 	}
 
 	// decide which agens we can skip
-	if ( stage->alphaGen == CGEN_IDENTITY ) {
+	if ( stage->alphaGen == (alphaGen_t)CGEN_IDENTITY ) {
 		if ( stage->rgbGen == CGEN_IDENTITY
 			 || stage->rgbGen == CGEN_LIGHTING_DIFFUSE ) {
 			stage->alphaGen = AGEN_SKIP;
@@ -1895,7 +1895,7 @@ static qboolean CollapseMultitexture( void ) {
 			return qfalse;
 		}
 	}
-	if ( stages[0].alphaGen == CGEN_WAVEFORM ) {
+	if ( stages[0].alphaGen == (alphaGen_t)CGEN_WAVEFORM ) {
 		if ( memcmp( &stages[0].alphaWave,
 					 &stages[1].alphaWave,
 					 sizeof( stages[0].alphaWave ) ) ) {
